a2729f44774948a09d7c4377c8cc6d7c6181213c
Dave <davidmcdonald40@gmail.com>
2021-02-13 15:51:32 -0700

    Changed some of the testing/ output in main.

--- lib/src/main/java/Driver.java
+++ lib/src/main/java/Driver.java
@@ -4,8 +4,11 @@ public class Driver {
 
         PushbackTokenizer i = new PushbackTokenizerImpl("This is a test!");
 
-       /* System.out.println("Printing i.nextToken(): ");
-        System.out.println(i.nextToken()); // should be "This"*/
+        System.out.println("Calling Pushback()");
+        i.pushback(); // Testing if nextToken hasn't been called.
+
+        System.out.println("Printing i.nextToken(): ");
+        System.out.println(i.nextToken()); // should be "This"
 
         System.out.println("Calling Pushback()");
         i.pushback();
