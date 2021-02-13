90b8e41522bda79222c28d32c8f94768dbd9e921
Dave <davidmcdonald40@gmail.com>
2021-02-13 15:30:51 -0700

    added PushbackTokenizer, PushbackTokenizerImpl and Driver

---
+++ lib/src/main/java/PushbackTokenizer.java
@@ -0,0 +1,10 @@
+
+// Class adaptor with StringTokenizer as adaptee
+public interface PushbackTokenizer {
+    // return the next token
+    String nextToken();
+    // returns true if and only if there are more tokens
+    boolean hasMoreTokens();
+    // the token read is pushed back, so it can be read again using nextToken.
+    void pushback();
+}
